094783ef4cf81a0b5ee5f7f6684e688c208ba9e7
Dave Holloway <dh@gn2-netwerk.de>
2012-09-11 13:13:53 +0200

    Initial Commit

---
+++ metadata.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Module information
+ */
+ 
+$aModule = array(
+    'id'           => 'tinyce',
+    'title'        => 'TinyCE WYSIWYG-Editor',
+    'description'  => 'WYSIWYG-Editor zur Textbearbeitung im Backend.',
+    'thumbnail'    => '',
+    'version'      => '1.0',
+    'author'       => 'GN2 netwerk',
+    'extend'       => array(
+        'oxoutput' => 'tinyce/tinyce_oxoutput'
+    )
+);
