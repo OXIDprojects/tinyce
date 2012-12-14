b768f08ca579c0713d4a9449d378345895e76c17
Dave Holloway <dh@gn2-netwerk.de>
2012-12-14 09:40:43 +0100

    'Automatic' language detection.

--- tinyce_oxoutput.php
+++ tinyce_oxoutput.php
@@ -35,14 +35,15 @@ class tinyce_oxoutput extends tinyce_oxoutput_parent
 
         if (in_array($class, $allowed))
         {
-
-
             $code = '';
 
-// Theme options - original
+            // Theme options - original
             // theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontselect,fontsizeselect",
             // theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
             // theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|fullscreen",
+
+            $bLangId = oxLang::getInstance()->getTplLanguage();
+            $backendLanguage = ($bLangId==0) ? "de" : "en";
 
             $code = <<<EOT
 <script type="text/javascript" src="../modules/tinyce/tiny_mce/tiny_mce.js"></script>
@@ -56,6 +57,7 @@ class tinyce_oxoutput extends tinyce_oxoutput_parent
     }
     tinyMCE.init({
         mode : "textareas",
+        language : "$backendLanguage",
         theme : "advanced",
         plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,iespell,insertdatetime,media,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave",
 
